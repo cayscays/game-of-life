c4a2c9de4b483656a56f9ba7ad54900628010f76
cayscays <116169018+cayscays@users.noreply.github.com>
2022-10-19 17:18:15 +0300

    Add files via upload

---
+++ Display/Label.java
@@ -0,0 +1,33 @@
+/**
+ * A text label which displays text and a changing counter.
+ * @author cayscays
+ * @version 1.0
+ * @since DEC.2020
+ */
+package GameOfLife.Display;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Label extends JLabel {
+    String constantText;
+
+    /**
+     * Constructs a Label.
+     * @param constantText  The part of the Label that stays the same during runtime.
+     * @param color         The text's color
+     */
+    public Label(String constantText, Color color) {
+        super(constantText);
+        this.constantText = constantText;
+        setForeground(color);
+    }
+
+    /**
+     * Updates the Label's visible text.
+     * @param counter   The counter to display.
+     */
+    public void update(int counter) {
+        setText(this.constantText + counter);
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Game of Life
+
+Based on [Conway's Game of Life](https://www.conwaylife.com/wiki/Conway%27s_Game_of_Life).
+
+Sit back and enjoy the ever-changing patterns!
